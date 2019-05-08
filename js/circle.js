5d8153abaa9877affa7358aef420096507f5c854
alextraykov <alextraykov@gmail.com>
2019-05-08 21:21:57 +0300

    First commit

---
+++ js/circle.js
@@ -0,0 +1,119 @@
+console.clear();
+
+const NUMBER_OF_SEGMENTS = 8;
+const SEGMENTS_MIN_RADIUS = 100;
+const SEGMENTS_MAX_RADIUS = 300;
+const ORIGIN_POINT = {
+  x: 250,
+  y: 270
+};
+const SHAPE_MOVEMENT_ALLOWED_RADIUS = {
+  min: 0.02,
+  max: 0.08
+};
+const LINE_WIDTH = 7;
+const LINE_COLOR = "rgba(218, 186, 114, 1)";
+
+class CircleSegment {
+  constructor(radius, numberOfSegments) {
+    const points = [];
+    const segmentsStep = (Math.PI * 2.35) / numberOfSegments;
+    for (let i = 0; i < numberOfSegments; i++) {
+      const x = ORIGIN_POINT.x + Math.sin(i * segmentsStep) * radius;
+      const y = ORIGIN_POINT.y + Math.cos(i * segmentsStep) * radius;
+      const timeOffset = (Math.random() * 2 - 1) * 2;
+      const moveRadius =
+        Math.random() * SHAPE_MOVEMENT_ALLOWED_RADIUS.max +
+        SHAPE_MOVEMENT_ALLOWED_RADIUS.min;
+      points.push({
+        x,
+        y,
+        timeOffset,
+        moveRadius
+      });
+    }
+    this._points = points;
+  }
+  update(dt, time) {
+    this._points.forEach((point, i) => {
+      point.x += Math.sin(time + point.timeOffset) * point.moveRadius;
+      point.y += Math.cos(time + point.timeOffset) * point.moveRadius;
+    });
+  }
+  render(ctx) {
+    ctx.save();
+    ctx.lineWidth = LINE_WIDTH;
+    ctx.strokeStyle = LINE_COLOR;
+    ctx.beginPath();
+    ctx.moveTo(this._points[0].x, this._points[0].y);
+    this._points.forEach((point, i) => {
+      const nextPoint = this._points[i + 1] || this._points[0];
+      const ctrlPoint = {
+        x: (point.x + nextPoint.x) / 2,
+        y: (point.y + nextPoint.y) / 2
+      };
+      ctx.quadraticCurveTo(point.x, point.y, ctrlPoint.x, ctrlPoint.y);
+    });
+    ctx.stroke();
+    ctx.restore();
+  }
+}
+
+class CircleAnimation {
+  constructor() {
+    this._shapes = [];
+  }
+  add(shape) {
+    this._shapes.push(shape);
+  }
+  update(dt, time) {
+    this._shapes.forEach(shape => shape.update(dt, time));
+    return this;
+  }
+  render(ctx) {
+    this._shapes.forEach(shape => shape.render(ctx));
+    return this;
+  }
+}
+
+let oldTime = 0;
+
+const domElement = document.querySelector("#my-container");
+
+const canvas = document.createElement("canvas");
+domElement.appendChild(canvas);
+
+const ctx = canvas.getContext("2d");
+
+const dpr = window.devicePixelRatio || 1;
+const {
+  width: domElWidth,
+  height: domElHeight
+} = domElement.getBoundingClientRect();
+
+canvas.width = domElWidth * dpr;
+canvas.height = domElHeight * dpr;
+canvas.style.width = `${domElWidth}px`;
+canvas.style.height = `${domElHeight}px`;
+
+const circleAnimationEffect = new CircleAnimation();
+
+for (let i = 0; i < NUMBER_OF_SEGMENTS; i++) {
+  const radiusDelta = SEGMENTS_MAX_RADIUS - SEGMENTS_MIN_RADIUS;
+  const currentRadius =
+    SEGMENTS_MIN_RADIUS + (i / NUMBER_OF_SEGMENTS) * radiusDelta;
+  const segment = new CircleSegment(currentRadius, NUMBER_OF_SEGMENTS);
+  circleAnimationEffect.add(segment);
+}
+
+const onRenderFrame = () => {
+  window.requestAnimationFrame(onRenderFrame);
+  const now = window.performance.now() / 1000;
+  const dt = now - oldTime;
+  oldTime = now;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  circleAnimationEffect.update(dt, now).render(ctx);
+};
+window.requestAnimationFrame(onRenderFrame);
---
+++ js/scroll.js
@@ -0,0 +1,75 @@
+// //1. Make some vars for our documentHeight and windowHeight
+// // 2.Make some vars to work out the diff between the two (as we're comparing this to our scrollTop positioin which goes from the top of the window) documentHeight - windowHeight
+// // 3. Using the diff and the scrollPsition, divide them into eachother to make a percentage
+// // 4. Multiply by 100 to get back % value
+
+// window.addEventListener('scroll', function (e) {
+//     const target = document.querySelector('.background-circle');
+//     var scrolled = window.pageYOffset;
+//     var rate = scrolled * -4;
+//     target.style.transform = 'translate3d(px' + rate + 'px, 0 px)';
+// });
+
+var parallaxElement1 = $("#my-container"),
+  parallaxQuantity = parallaxElement1.length;
+
+$(window).on("scroll", function() {
+  window.requestAnimationFrame(function() {
+    for (var i = 0; i < parallaxQuantity; i++) {
+      var currentElement = parallaxElement1.eq(i);
+      var scrolled = $(window).scrollTop();
+
+      currentElement.css({
+        transform: "translate3d(0," + scrolled * 0.2 + "px, 0)"
+      });
+    }
+  });
+});
+
+var parallaxElement2 = $(".background-circle"),
+  parallaxQuantity = parallaxElement2.length;
+
+$(window).on("scroll", function() {
+  window.requestAnimationFrame(function() {
+    for (var i = 0; i < parallaxQuantity; i++) {
+      var currentElement = parallaxElement2.eq(i);
+      var scrolled = $(window).scrollTop();
+
+      currentElement.css({
+        transform: "translate3d(0," + scrolled * 0.15 + "px, 0)"
+      });
+    }
+  });
+});
+
+var parallaxElement3 = $(".background-circle2"),
+  parallaxQuantity = parallaxElement3.length;
+
+$(window).on("scroll", function() {
+  window.requestAnimationFrame(function() {
+    for (var i = 0; i < parallaxQuantity; i++) {
+      var currentElement = parallaxElement3.eq(i);
+      var scrolled = $(window).scrollTop();
+
+      currentElement.css({
+        transform: "translate3d(0," + scrolled * 0.35 + "px, 0)"
+      });
+    }
+  });
+});
+
+var parallaxElement4 = $(".background-circle3"),
+  parallaxQuantity = parallaxElement4.length;
+
+$(window).on("scroll", function() {
+  window.requestAnimationFrame(function() {
+    for (var i = 0; i < parallaxQuantity; i++) {
+      var currentElement = parallaxElement4.eq(i);
+      var scrolled = $(window).scrollTop();
+
+      currentElement.css({
+        transform: "translate3d(0," + scrolled * 0.1 + "px, 0)"
+      });
+    }
+  });
+});
